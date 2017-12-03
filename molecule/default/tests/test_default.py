cdcda49ec457ea03cc522fb7fe03e56d1412814f
Stanislav Popov <popstas@gmail.com>
2017-12-03 06:44:33 +0500

    test: testinfra

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,11 +1,12 @@
+import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    '.molecule/ansible_inventory').get_hosts('all')
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_crontab_file(Command):
-    c = Command('crontab -l').stdout
+def test_crontab_file(host):
+    c = host.command('crontab -l').stdout
     assert 'MAIL=mail@example.com' in c
     assert '#Ansible: log env' in c
     assert '1 * * * * env > /tmp' in c
